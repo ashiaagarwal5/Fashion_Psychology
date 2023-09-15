5107274f5a27954ed651da3a553e2c3e9bce29bd
ashiaagarwal5 <86387528+ashiaagarwal5@users.noreply.github.com>
2023-09-14 23:27:35 -0500

    Add files via upload

---
+++ Fashion_Slideshow.js
@@ -0,0 +1,22 @@
+// JavaScript for the slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slideshow img');
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = 'block';
+    
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds (adjust as needed)
+}
+
